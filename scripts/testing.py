a0f3a460f78883b0f9f7cc7689d1b902dc40e07c
Garvgupta06 <garvgupta2901@gmail.com>
2025-11-25 08:43:28 +0530

    Initial project upload

---
+++ scripts/testing.py
@@ -0,0 +1,50 @@
+import joblib
+import pandas as pd 
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix
+
+model = joblib.load('D:/ml project/aviation_incident_model.pkl')
+print("model loaded sucessfully!")
+
+df = pd.read_csv("D:/ml project/data/final_aviation_data.csv")
+print("data loaded succesfully! Rows: ", len(df))
+
+
+test_rows = int(input("eneter rows you want to sample for testing:"))
+print(f"sampling {test_rows} rows for testing..")
+sample = df.sample(test_rows, random_state = 42)
+true_labels = sample["target"]
+sample_input = sample.drop(columns = ["target"])
+
+print("\nSAMPLE INPUT")
+print(sample_input)
+preds= model.predict(sample_input)
+probs = model.predict_proba(sample_input)[:,1]
+
+print("\nPredictions:")
+print(preds)
+
+print("\nProbability(Fatal Risk Percentage): ")
+print(probs)
+
+print("\nTrue Labels: ")
+print(true_labels.values)
+
+from sklearn.model_selection import train_test_split
+
+X = df.drop(columns=["target"])
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+y_pred = model.predict(X_test)
+
+print("\n=== CLASSIFICATION REPORT ===")
+print(classification_report(y_test, y_pred))
+
+print("\n=== CONFUSION MATRIX ===")
+print(confusion_matrix(y_test, y_pred))
+
+print("\n✔ MODEL TESTING COMPLETE!")
